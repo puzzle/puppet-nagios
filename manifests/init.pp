48b91f5466ad314db9d908866fe01610e7d5cb7c
Marcel Haerry <haerry@puzzle.ch>
2008-04-22 11:58:27 +0200

    empty module $MODULENAME

---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+#######################################
+# nagios module
+# Puzzle ITC - haerry+puppet(at)puzzle.ch
+# GPLv3
+#######################################
+
+
+# modules_dir { "nagios": }
+class nagios {}
