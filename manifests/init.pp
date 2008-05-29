e956629bcdba61a3f93cc0cd0bb5ada50093e694
Simon Josi <sjosi@pw-sjosi.puzzle.itc>
2008-05-29 18:39:42 +0200

    added puzzle copyright notice

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,18 @@
+#
+# nagios module
 # nagios.pp - everything nagios related
+#
 # Copyright (C) 2007 David Schmitt <david@schmitt.edv-bus.at>
-# See LICENSE for the full license granted to you.
-# adapted and improved by admin(at)immerda.ch
-# adapted by Puzzle ITC - haerry+puppet(at)puzzle.ch
-
+# Copyright 2008, admin(at)immerda.ch
+# Copyright 2008, Puzzle ITC GmbH
+# Marcel Härry haerry+puppet(at)puzzle.ch
+# Simon Josi josi+puppet(at)puzzle.ch
+#
+# This program is free software; you can redistribute 
+# it and/or modify it under the terms of the GNU 
+# General Public License version 3 as published by 
+# the Free Software Foundation.
+#
 
 # the directory containing all nagios configs:
 $nagios_cfgdir = '/var/lib/puppet/modules/nagios'
